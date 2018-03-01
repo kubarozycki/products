04a10d3174c7e25182b6906b204e459ad3afd8be
Jakub Rozycki <jakubrozycki@outlook.com>
2018-03-01 17:15:30 +0100

    Initial commit

---
+++ web1/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+public class ProductsController:Controller
+{
+    public List<Product> GetAllProducts(string searchedPhrase)
+    {
+        SqlConnection connection=new SqlConnection();
+        connection.ConnectionString="Server=WIN-I8619RLSHP\\SQLEXPRESS;Database=TSQL2012;Trusted_Connection=True;";
+        connection.Open();
+        SqlCommand command=new SqlCommand();
+        
+        command.CommandType=CommandType.Text;
+        command.CommandText=$"SELECT * FROM GetAllProducts WHERE productname like '%{searchedPhrase}%'";
+        command.Connection=connection;
+        SqlDataReader reader = command.ExecuteReader();
+        
+        List<Product> productNames=new List<Product>();
+        while (reader.Read())
+        {
+             Product temp=new Product();
+             temp.ProductID=int.Parse(reader["productid"].ToString());
+             temp.ProductName=reader["productName"].ToString();
+             
+             productNames.Add(temp);
+        }
+        
+        return productNames;
+    }
+}
