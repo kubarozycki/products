95cb2d0a7f22cbe9ee67043eaf3bd0ac58b27b5c
Jakub Rozycki <jakubrozycki@outlook.com>
2018-03-12 19:56:05 +0100

    Sorting

--- web1/Controllers/ProductsController.cs
+++ web1/Controllers/ProductsController.cs
@@ -4,19 +4,26 @@ using System.Data;
 using System.Data.SqlClient;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using web1.Models;
+
 
 public class ProductsController:Controller
 {
     [HttpGet]
-    public List< Product> GetAllProducts(string searchedPhrase,int categoryid)
+    public ViewResult GetAllProducts(string sortBy)
     { 
+        List<Product> products=new List<Product>();
         SqlConnection connection=new SqlConnection();
         connection.ConnectionString="Server=WIN-I8619RLSHP\\SQLEXPRESS;Database=TSQL2012;Trusted_Connection=True;";
         connection.Open();
+        
         SqlCommand command=new SqlCommand();
         
+        if(sortBy==null){
+            sortBy="ProductName";
+        }
         command.CommandType=CommandType.Text;
-        command.CommandText=$"SELECT * FROM GetAllProducts WHERE productname like '%{searchedPhrase}%' AND categoryid={categoryid} ";
+        command.CommandText=$"SELECT * FROM GetAllProducts order by {sortBy}";
         command.Connection=connection;
         SqlDataReader reader = command.ExecuteReader();
         
@@ -26,18 +33,15 @@ public class ProductsController:Controller
              Product temp=new Product();
              temp.ProductID=int.Parse(reader["productid"].ToString());
              temp.ProductName=reader["productName"].ToString();
-             
+             temp.CategoryID=int.Parse(reader["categoryid"].ToString());
+             temp.UnitPrice=double.Parse(reader["unitprice"].ToString());
              productNames.Add(temp);
         }
-        
-        return productNames;
+
+        ViewData["products"]=productNames;
+        return View();   
     }
 
-    [HttpPost]
-    public Product AddProduct([FromBody] Product product)
-    {
-        
-        return product;
-    } 
+    
    
 }
